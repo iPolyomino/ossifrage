2d97ed73443a74b36d04f568129f9d8dce5b3d6e
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-06-02 03:34:54 +0900

    set url by argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+use std::env;
+use std::process;
+
 use ossifrage::fetch::fetch_document;
 use ossifrage::parse::{html2dom, walk};
 use ansi_term::Style;
 
 #[tokio::main]
 async fn main() {
-    let response = fetch_document().await;
-    let html = response.unwrap();
-    let dom = html2dom(&html);
-    walk(0, &dom.document, &Style::new());
+    let args : Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("please set url in argument");
+        process::exit(1);
+    }
+    let url : &str = &args[1];
+    match fetch_document(&url).await {
+        Ok(response) => {
+            let dom = html2dom(&response);
+            walk(0, &dom.document, &Style::new());
+        },
+        Err(_) => {
+            println!("cannot access to {}", url);
+            process::exit(1);
+        }
+    }
 }
