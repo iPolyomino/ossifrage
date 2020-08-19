7f41306d648f118087dcd52e03ddb4f923f08371
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-08-19 11:24:51 +0900

    update: main.rs stdout

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 use std::env;
+use std::io::{self, Write};
 use std::process;
 
+use ansi_term::Style;
 use ossifrage::fetch::fetch_document;
 use ossifrage::parse::{html2dom, walk};
-use ansi_term::Style;
 
 #[tokio::main]
 async fn main() {
-    let args : Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("please set url in argument");
+    let mut url = String::new();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        url = String::from(&args[1]);
+    }
+    if url.is_empty() {
+        io::stdin().read_line(&mut url).unwrap();
+    }
+
+    if url.is_empty() {
+        io::stdout()
+            .write("please set url in argument".as_bytes())
+            .unwrap();
         process::exit(1);
     }
-    let url : &str = &args[1];
+
     match fetch_document(&url).await {
         Ok(response) => {
             let dom = html2dom(&response);
             walk(0, &dom.document, &Style::new());
-        },
+        }
         Err(_) => {
             println!("cannot access to {}", url);
             process::exit(1);
